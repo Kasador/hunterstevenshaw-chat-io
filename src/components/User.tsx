043099cb8249210692c6fc65d1497c7d8707956f
Kasador <hunterstevenshaw@gmail.com>
2025-04-20 15:50:56 -0700

    Updated UI/UX & README.md.

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,13 +3,13 @@ import { FaCircleUser } from "react-icons/fa6";
 export default function User() {
     return (
         <section className="h-50 w-100 bg-amber-100 border-2 shadow-2xl rounded-2xl p-4 m-10 flex flex-col">
-            <div className="flex items-center my-2">
+            <div className="flex items-center">
             <FaCircleUser />
-            <label className="font-extrabold px-2">Choose a Username</label>
+            <label className="font-extrabold px-2 text-lg">Choose a Username</label>
             </div>
-            <input type="text" className="w-50 bg-amber-200 border-1 rounded-2xl"/>
+            <input type="text" className="w-91 bg-amber-200 border-1 rounded-2xl p-2 mt-5"/>
                 {/* <input type="submit" value="Submit" /> */}
-            <button type="button" className="font-extrabold my-10 p-4 w-25 bg-amber-500 border-2 rounded-b-full cursor-pointer">Join</button>
+            <button type="button" className="font-extrabold my-3 p-4 bg-amber-500 border-2 rounded-b-full cursor-pointer hover:bg-amber-400 hover:shadow-2xl duration-200 ease-in-out">Join</button>
         </section>
     )
 }
